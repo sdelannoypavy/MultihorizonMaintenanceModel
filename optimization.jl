d137e043750a48760da785bd3d908faa5f971ed9
sdelannoypavy <solene.delannoypavy_externe@rte-france.com>
2025-05-29 22:43:43 +0200

    add parameters

--- optimization.jl
+++ optimization.jl
@@ -3,22 +3,42 @@ using Gurobi
 using JuMP  
 using Distributions
 
-function V(p,S)
+function V(p,S,nb_state)
 
-    V_table = [0.0, 0.0]
+    V_table = [0.0 for _ in 1:nb_state]
 
     #p = 0.0005
 
-    P_w = [[1.0 - p, 0.0],
-           [p, 1.0]] 
-    P_m = [[1.0, 1.0],
-           [0.0, 0.0]] 
+    #P_w = [[1.0 - p, 0.0],
+    #       [p, 1.0]] 
 
-    x_0 = [1.0, 0.0]
+    P_w = zeros(nb_state, nb_state)
 
-    C_x = [100.0, 0.0]
+    for i in 1:(nb_state - 2)
+        P_w[i,i] = 1.0 - 2*p 
+        P_w[i+1,i] = p
+        P_w[nb_state,i] = p
+    end
 
-    nb_state = 2
+    P_w[nb_state - 1,nb_state - 1] = 1.0 - p 
+    P_w[nb_state, nb_state - 1] = p
+
+    P_w[nb_state,nb_state] = 1.0
+
+    #P_m = [[1.0, 1.0],
+    #       [0.0, 0.0]] 
+
+    P_m = zeros(nb_state, nb_state)
+
+    for i in 1:(nb_state)
+        P_m[1,i] = 1.0 
+    end
+
+    x_0 = [1.0; zeros(nb_state - 1)]
+
+    C_x = [100.0 - (i-1)*100/(nb_state-1) for i in 1:nb_state]
+
+    #nb_state = 2
 
     T = 30
     #S = 3
@@ -48,7 +68,7 @@ function V(p,S)
     @objective(model, Min, sum(c[t] for t in 1:T) + sum(x[S+1,T+1,i]*V_table[i] for i in 1:nb_state))
 
 
-    @constraint(model, [s in 1:(S+1), t in 2:(T+1), i in 1:nb_state], x[s,t,i] == (1-u[s,t-1])*(P_w[i][1]*x[s,t-1,1]+P_w[i][2]*x[s,t-1,2]) + u[s,t-1]*(P_m[i][1]*x[s,t-1,1]+P_m[i][2]*x[s,t-1,2]))
+    @constraint(model, [s in 1:(S+1), t in 2:(T+1), i in 1:nb_state], x[s,t,i] == (1-u[s,t-1])*sum(P_w[i,j]*x[s,t-1,j] for j in 1:nb_state) + u[s,t-1]*sum(P_m[i,j]*x[s,t-1,j] for j in 1:nb_state))
 
     @constraint(model, [s in 1:(S+1), i in 1:nb_state], q[s,1] == d_c*m[1])
     @constraint(model, [s in 1:(S+1), t in 1:(T-1)], q[s,t+1] == q[s,t] - u[s,t] + d_c*m[t+1])
@@ -63,7 +83,7 @@ function V(p,S)
     @constraint(model, sum(k[t] for t in 1:T) <= K)
 
     @constraint(model, [s in 1:S, t in 1:T, i in 1:nb_state], p_evac[s,t,i] >= prod[s,t] - C_x[i])
-    @constraint(model, [t in 1:T], c[t] >= (1/S)*sum((1 - u[s,t])*(x[s,t,1]*p_evac[s,t,1] + x[s,t,2]*p_evac[s,t,2]) + ong_m[s,t]*(1-k[t])*prod[s,t] for s in 1:S))
+    @constraint(model, [t in 1:T], c[t] >= (1/S)*sum((1 - u[s,t])*sum(x[s,t,i]*p_evac[s,t,1] for i in 1:nb_state) + ong_m[s,t]*(1-k[t])*prod[s,t] for s in 1:S))
 
 
 
@@ -83,7 +103,7 @@ function V(p,S)
         #u = [value(u[s,t]) for  s in 1:S, t in 1:T]
         #q = [value(q[s,t]) for  s in 1:S, t in 1:T]
         #ong_m = [value(ong_m[s,t]) for  s in 1:S, t in 1:T]
-        #x = [value(x[s,t,2]) for  s in 1:S, t in 1:(T+1)]
+        #x = [value(x[s,t,nb_states]) for  s in 1:S, t in 1:(T+1)]
 
         return(numvars,numcons)
     else
--- script_290525.jl
+++ script_290525.jl
@@ -6,9 +6,6 @@ include("optimization.jl")
 
 fichier_csv = "computing_time.csv"
 
-Random.seed!(1)
-
-
 # Pfixed parameters
 P0 = 100
 Nb_scen_prod = 1
@@ -16,21 +13,25 @@ Quota = 1
 
 res = []
 
-function test(fail_prob_list, S_list)
+function test(fail_prob_list, S_list, nb_state_list)
     # warning: Ns is number of scenarios for each jumb probability, not total number of scenarios
 
     for p in fail_prob_list
         res_T = []
         
         for S in S_list
-            local t_start = now()
-            
-            num_vars, num_constrs = V(p,S)
 
-            local t_end = now()
-            local t_elapsed = floor(Int,(t_end - t_start)/Second(1))
-            push!(res_T, t_elapsed)
-            CSV.write(fichier_csv, [(; param1=p, param3=S, param3=num_vars, param4=num_constrs)], append=true)
+            for nb_state in nb_state_list
+                local t_start = now()
+                
+                num_vars, num_constrs = V(p,S,nb_state)
+
+                local t_end = now()
+                local t_elapsed = floor(Int,(t_end - t_start)/Second(1))
+                push!(res_T, t_elapsed)
+                CSV.write(fichier_csv, [(; param1=p, param2=S, param3=nb_state, param4=num_vars, param5=num_constrs, param6=t_elapsed)], append=true)
+            end
+
         end
         push!(res, res_T)
 
@@ -39,7 +40,8 @@ function test(fail_prob_list, S_list)
 end
 
 # testing parameters
-fail_prob_list = [0.0005]
-S_list = [1,3]
+fail_prob_list = [0.05]
+S_list = [1,5,10]
+nb_state_list = [2]
 
-test(fail_prob_list, S_list)
+test(fail_prob_list, S_list,nb_state_list)
